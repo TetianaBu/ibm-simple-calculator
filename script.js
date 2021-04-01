d74a9cfc61b8ac82dceab3035322f2c1c3c88391
TetianaBu <57547488+TetianaBu@users.noreply.github.com>
2021-04-01 13:59:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,39 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+const button = document.querySelector('.button');
+const results = document.querySelector('.results');
+const PRINCIPAL = document.querySelector('#principal');
+const YEARS = document.querySelector('#years');
+const CLIENT_DEPOSIT = document.querySelector('.client-deposit');
+const TIME = document.querySelector('.time');
+const TOTAL_INTERES = document.querySelector('.interest-rate');
+const TOTAL_AMOUNT = document.querySelector('.total-amount');
+
+let slider = document.getElementById('rate');
+let output = document.getElementById('range-p');
+output.innerHTML = slider.value;
+slider.oninput = function () {
+  output.innerHTML = this.value;
+};
+
+function calculateResults(e) {
+  const p = parseFloat(PRINCIPAL.value);
+  const r = parseFloat(slider.value);
+  const t = parseFloat(YEARS.value);
+  const interest = (p * t * r) / 100;
+  const total = (interest + p).toFixed(2);
+  const dateNow = new Date();
+  const calculateYear = parseInt(dateNow.getFullYear()) + parseInt(YEARS.value);
+  if (isFinite(total)) {
+    CLIENT_DEPOSIT.innerHTML = `If you deposit <mark>${p}$</mark>`;
+    TOTAL_INTERES.innerHTML = `at an interest rate of <mark>${r}%</mark>`;
+    TOTAL_AMOUNT.innerHTML = `You will receive an amount of <mark>${total}$</mark>`;
+    TIME.innerHTML = `in the year <mark>${calculateYear}</mark>`;
+    results.classList.remove('hide');
+  } else {
+    alert('Please check your numbers and try again.');
+  }
 }
-        
+
+button.addEventListener('click', (e) => {
+  setTimeout(calculateResults, 2000);
+  e.preventDefault();
+});
